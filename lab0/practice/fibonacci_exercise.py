5d31cb6f2124583cf54677d6dd576f4a6824e0b5
lydia-miller <lydia.miller1@gwmail.gwu.edu>
2026-08-31 11:14:29 -0400

    lab0 practice

--- lab0/practice/fibonacci_exercise.py
+++ lab0/practice/fibonacci_exercise.py
@@ -6,8 +6,15 @@ def fibonacci(n):
         raise ValueError("Fibonacci is not defined for negative integers")
 
     # TODO: Add the base case. Note: Fib(0) is 0; Fib(1) is 1; Fib(2) is 1, so on.
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
 
     # TODO: Return fibonacci: 
+    return fibonacci(n-1) + fibonacci(n-2)
     raise NotImplementedError("Complete fibonacci")
 
 
